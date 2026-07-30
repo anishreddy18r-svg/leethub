9d89e57624e96900da3d0ab6d8b5efea5fd75348
anishreddy18r-svg <anish.reddy.18r@gmail.com>
2026-07-30 08:50:01 +0530

    Time: 2 ms (89.91%), Space: 44.2 MB (56.46%) - LeetHub - Time: O(m*n), Space: O(m*n)

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -13,22 +13,17 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         Queue<Pair>q=new LinkedList<>();
-        int[][]vis=new int[m][n];
         int fresh=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    vis[i][j]=0; 
                     fresh++;
                 }
                 else if(grid[i][j]==2){
                     Pair p=new Pair(i,j,0);
                     q.offer(p);
-                    vis[i][j]=2;
                 }
-                else{
-                    vis[i][j]=0;
-                }
+                
             }
         }
         
@@ -36,16 +31,17 @@ class Solution {
         int[]dc={0,1,0,-1};
         int max=0;
         while(!q.isEmpty()){
-            int r=q.peek().a;
-            int c=q.peek().b;
-            int t=q.peek().tm;
+            Pair p=q.poll();
+            int r=p.a;
+            int c=p.b;
+            int t=p.tm;
             max=Math.max(max,t);
-            q.remove();
+            
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && vis[nr][nc]==0){
-                    vis[nr][nc]=2;
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                    grid[nr][nc]=2;
                     q.add(new Pair(nr,nc,t+1));
                     fresh--;
                 }
