0ad4cbd6fb389b757fde0b39b0b6f862416ecb91
anishreddy18r-svg <anish.reddy.18r@gmail.com>
2026-08-22 19:16:40 +0530

    Time: 23 ms (91.31%), Space: 44.1 MB (72.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,19 @@
 class Solution {
-    public boolean f(int i,int target,int []nums,Boolean[][]dp){
-        if(i<0||target<0) return false;
-        if(target==0) return true;
-        if(nums[i]==target) return true;
-        if(dp[i][target]!=null) return dp[i][target];
-        boolean take=f(i-1,target-nums[i],nums,dp);
-        boolean notTake=f(i-1,target,nums,dp);
-        return dp[i][target]=take||notTake;
-    }
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int num:nums) sum+=num;
         if(sum%2!=0) return false;
-        int target=sum/2;
-        int n=nums.length;
-        Boolean dp[][]=new Boolean[n][target+1];
-
-        return f(n-1,target,nums,dp);
+        int req=sum/2;
+        
+        boolean[] dp=new boolean[req+1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int k=req;k>=num;k--){
+                dp[k]=dp[k]||dp[k-num];
+            }
+            if(dp[req]) return true;
+        }
+        return dp[req];
     }
 }
 
